f100165d31e4e338fbd171ae421f296080e60740
samsung <samsung@DESKTOP-BBUA1T1>
2023-06-07 08:42:59 +0900

    초기 커밋.

---
+++ MyFirstCSharp/Chgap19_Foreach_Test_T[Foreach 예제].cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MyFirstCSharp
+{
+    public partial class Chgap19_Foreach_Test_T : Form
+    {
+        public Chgap19_Foreach_Test_T()
+        {
+            InitializeComponent();
+        }
+
+        private void btnRun_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
