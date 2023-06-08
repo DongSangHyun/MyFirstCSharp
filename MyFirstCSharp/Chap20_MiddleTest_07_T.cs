4f2c133dcb5431f8e7c2b3ebf72746cc4faa1358
samsung <samsung@DESKTOP-BBUA1T1>
2023-06-08 14:24:05 +0900

    230608_1424

--- MyFirstCSharp/Chap20_MiddleTest_07_T.cs
+++ MyFirstCSharp/Chap20_MiddleTest_07_T.cs
@@ -27,14 +27,15 @@ namespace MyFirstCSharp
             int[] iValues = new int[20];
             
             // i : iValues 의 index
-            for (int i  =0; i < 20; i++)
+            for (int i  =0; i < iValues.Length; i++)
             {
-                iValues[i] = random.Next(0, 21); // 20 미만. 
+                iValues[i] = random.Next(0, 21); //  0<= x < 21   0 ~ 20
             }
 
             //  텍스트 박스에 오름차순으로 표현하기
-            Array.Sort(iValues);
-            txtResult.Text = "";
+            Array.Sort(iValues); 
+            
+            txtResult.Text = ""; // textbox 초기화
             foreach (int i in iValues)
             {
                 txtResult.Text += i.ToString() + " ";
@@ -48,35 +49,35 @@ namespace MyFirstCSharp
             for (int i = 0; i < 21; i++)
             {
                 #region < For 문을 이용한 값의 존재 여부 확인. >
-                //// 찾을 기준값으로 배열에 데이터가 있는지 확인.
-                //// j : 배열의 index 
-                //int j = 0;
-                //for (j = 0; j < iValues.Length; j++)
-                //{
-                //    if (iValues[j] == i)
-                //    {
-                //        // 배열에 i 값이 할당 되어 있다.  
-                //        break;
-                //    }
-                //}
-                //if (j == 20)
-                //{
-                //    // 누락 된 값을 결과에 등록.
-                //    iResult += i; // 누적 합산 결과
-                //    sResult.Append(i + " "); // 누락 된 수 리스트.
-                //}
+                // 찾을 기준값으로 배열에 데이터가 있는지 확인.
+                // j : 배열의 index 
+                int j = 0;
+                for (j = 0; j < iValues.Length; j++)
+                {
+                    if (iValues[j] == i)
+                    {
+                        // 배열에 i 값이 할당 되어 있다.  
+                        break;
+                    }
+                }
+                if (j == 20)
+                {
+                    // 누락 된 값을 결과에 등록.
+                    iResult += i; // 누적 합산 결과
+                    sResult.Append(i + " "); // 누락 된 수 리스트.
+                }
                 #endregion
 
                 #region < 기능을 통하여 찾아내기 >
-                // 배열에 값이 있는지 찾기. ( i 값이 있는지 찾기 ) 
-                // indexof  : 배열에 찾을 값이 없을경우 -1
-                int iIndexFlag = Array.IndexOf(iValues,i);
-                if (iIndexFlag == -1)  
-                {
-                    // 찾는 값이 배열에 없으므로 
-                    iResult += i;
-                    sResult.Append(i + " ");
-                }
+                //// 배열에 값이 있는지 찾기. ( i 값이 있는지 찾기 ) 
+                //// indexof  : 배열에 찾을 값이 없을경우 -1
+                //int iIndexFlag = Array.IndexOf(iValues,i);
+                //if (iIndexFlag == -1)  
+                //{
+                //    // 찾는 값이 배열에 없으므로 
+                //    iResult += i;
+                //    sResult.Append(i + " ");
+                //}
                 #endregion
             }
 
