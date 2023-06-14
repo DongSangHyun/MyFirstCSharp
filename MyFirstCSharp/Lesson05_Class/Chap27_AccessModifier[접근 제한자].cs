d5660f89dd4c31d87ed454c3160303abc1bedc11
samsung <samsung@DESKTOP-BBUA1T1>
2023-06-14 11:18:11 +0900

    230614_1118

--- MyFirstCSharp/Lesson05_Class/Chap27_AccessModifier[접근 제한자].cs
+++ MyFirstCSharp/Lesson05_Class/Chap27_AccessModifier[접근 제한자].cs
@@ -37,8 +37,62 @@ namespace MyFirstCSharp.Lesson05_Class
         // Protected : 파생 클래스에서 접근이 가능. 
 
         // 클래스의 인스턴스 변수  ( 클래스의 필드 멤버, 전역변수) 
-        public string sPublic = "안녕하세요";        // 모든 곳에서 접근이 가능한 인스턴스 변수.
-        private string sPrivate = "Private string"; // 해당 클래스에서만 호출이 가능한 인스턴스 변수.
+        public    string sPublic  = "안녕하세요";           // 모든 곳에서 접근이 가능한 인스턴스 변수.
+        private   string sPrivate = "Private string";       // 해당 클래스에서만 호출이 가능한 인스턴스 변수.
         protected string sPartial = "Partial Class string"; // 분할 클래스 에서만 접근이 가능한 인스턴스 변수
+
+        //int iLoopCnnt = 0;
+
+
+        public void FindValue()
+        {
+            // 다른 클래스에서 호출이 가능한 public 형태의 메서드 
+            sPrivate = "클래스 내에서 접근이 가능함";
+
+            FindValue2(); // 해당 클래스 내에서 호출 가능한 private 메서드
+        }
+
+        private void FindValue2()
+        {
+            //  다른 클래스에서는 호출 할 수 없는  private 메서드.
+            sPrivate = "동일 클래스의 다른 메서드 에서도 접근 가능";
+            sPublic  = "동일 클래스의 다른 메서드 에서도 접근 가능";
+            sPartial = "동일 클래스의 다른 메서드 에서도 접근 가능";
+        }
+
     }
+
+
+    // 클래스의 코드 작성 위치
+    // . 클래스를 생성할 경우 프로젝트 에 cs 파일로 추가 하는 방법이 있고. 
+    //   소스 내에서 클래스를 수동으로 생성 하는 방법이 있다. 
+    //   여러 개발자가 개발을 할 경우 공통으로 사용하는 클래스 인 경우 
+    //   솔루션 탐색기 에서 확인 할 수 있도록 파일 형태의 클래스를 생성 하는것이 좋고
+    //   개발자 가 단독으로 사용할 클래스 인 경우 소스 내에서 생성하여 사용 할수 있다. 
+    class NewClass
+    { 
+        // 새로운 클래스의 생성.
+        
+        void NewMethod()
+        {
+            // 로직이 실행되는 메서드 
+            Random randonm = new Random();  // 마이크로 소프트 에서 만들어서 제공하는 클래스.
+            Chap27_AccessModifier CHP27 = new Chap27_AccessModifier(); // 내가 만들어서 사용할 클래스.
+            CHP27.sPublic = "반갑습니다.";
+            //CHP27.sPrivate = "외부에서 접근 불가.";
+            //CHP27.sPartial = "외부에서 접근 불가";
+
+            CHP27.FindValue(); // 다른 클래스에서 접근이 가능한 메서드 public Method
+            //CHP27.FindValue2(); // 다른 클래스에서 접근이 불가능한 메서드 private Method
+
+            // FindValue() 를 실행하고 FindValue2() 를 순차적으로 실행해야 하는경우
+            // FindValue2() 를 외부에서 접근하지 못하도록 Private 로 접근제한을 두고
+            // FindValue() 는 외부에서 접근 가능하도록 Public 으로 접근 제한을 두면서 FindValue2() 를 호출 함으로서
+            // 순차적인 로직을 실행 할 수 있도록 한다. 
+            
+            // 캡술화
+            // 최소한 의 소스, 코딩 만 외부에 공개 함으로서 프로그램의 무결성과 안전성을 유지 하도록 하는 방식 
+            
+        }
+    }  
 }
---
+++ MyFirstCSharp/Lesson05_Class/Chap27_Accessodifier_Partial.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFirstCSharp.Lesson05_Class
+{
+    // Partial Class (분할 클래스 , 파생 클래스)
+    // . 클래스는 동일한 NameSpace 에서 같은이름으로 작성될 수 없다. 
+    // . 여러명이 같은 클래스를 동시에 개발 하거나.
+    //   단위 모듈별로 코딩을 나누어 개발해야 하는 경우 
+    //   같은 클래스임을 표현하는 키워드
+    partial class Chap27_Accessodifier_Partial
+    {
+    }
+
+    partial class Chap27_Accessodifier_Partial
+    {
+        // 같은 이름의 클래스는 두개 이상 만들수 없다.
+        // partial Class 를 선언하면 
+        // 같은이름으로 두개의 클래스를 생성 할수 있다. 
+        // 이때 * 클래스가 두개가 생성 된게 아니라. 
+        // 하나의 클래스임을 선언하는 키워드 가 Partial
+    }
+}
