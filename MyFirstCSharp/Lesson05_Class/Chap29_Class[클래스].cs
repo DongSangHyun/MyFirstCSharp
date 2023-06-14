f3275638fff2cbc9bcdd23669dfe6c37b735c637
samsung <samsung@DESKTOP-BBUA1T1>
2023-06-14 15:22:51 +0900

    230614_1522

---
+++ MyFirstCSharp/Lesson05_Class/Chap29_Class[클래스].cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFirstCSharp.Lesson05_Class
+{
+    /*
+       클래스 
+       . 객체 지향 프로그래밍 언어를 이루는 가장 중요한 개념. 
+       . 프로그램을 구현하는 설계되 역할 (설계도를 참조해서 실체를 만들어내는 과정)
+       . 설계도를 보고 같은 모양의 단위 부품을 필요에 의해 여러번 만들 수 있듯이
+         클래스 라는 파일을 생성해 두고 프로그램 내에서 필요에 따라 
+         객체 (실체 ) 로 인스턴스 화 해서 사용할 수 있다.
+       . 객체화(인스턴스화) : 클래스(설계도, 레시피) 를 바탕으로 실제로 구현 되어야할
+                               내용을 실행 할 수 있도록 만드는 단계 
+       . 외부에서 선언후 사용하는 것이 목적 기본 상태는 Public 
+         Private 형태로는 만들수 없다. (중첩 클래스 일경우는 Private 제한자 설정이 가능)
+    */
+    
+
+    internal class Chap29_Class
+    {
+    }
+}
